aa186ca09d42deee9af9709d2dcebcef8d1b4951
ianbridges <megabridge12@gmail.com>
2022-07-14 18:22:37 -0500

    employee test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('creates employee object', () => {
+    const employee = new Employee('Ian', 1, 'ian@email.com');
+
+    expect(employee.name).tobe('Ian');
+    expect(employee.id).tobe(1);
+    expect(employee.email).tobe('ian@email.com');
+});
