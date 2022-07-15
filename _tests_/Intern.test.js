5ac478efbb3d1cfd726ad7a2631f46242a6b473f
ianbridges <megabridge12@gmail.com>
2022-07-15 16:43:27 -0500

    intern test

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Engineer');
+
+test('creates intern object', () => {
+    const intern = new Intern('Ian', 1, 'ian@email.com', 'Washington University');
+
+    expect(intern.name).tobe('Ian');
+    expect(intern.id).tobe(1);
+    expect(intern.email).tobe('ian@email.com');
+    expect(intern.school).tobe('Washington University');
+});
