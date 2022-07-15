3976b3ebb4856a112a1cbeee0c902bd159a07cca
ianbridges <megabridge12@gmail.com>
2022-07-15 18:04:31 -0500

    index.js linking

---
+++ index.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+import fs from "fs";
+
+import Engineer from '../Ian-s-Team-Profile-Generator/lib/Engineer.js'
+import Intern from '../Ian-s-Team-Profile-Generator/lib/Intern.js'
+import Manager from '../Ian-s-Team-Profile-Generator/lib/Manager.js'
+
+import renderData from '../Ian-s-Team-Profile-Generator/src/renderData.js'
