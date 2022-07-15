d886b04c20680a356dd03c93741085a10097d880
ianbridges <megabridge12@gmail.com>
2022-07-15 17:32:14 -0500

    employee class

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,6 +1,6 @@
 const Manager = require('../lib/Manager');
 
-test('creates intern object', () => {
+test('creates manager object', () => {
     const manager = new Manager('Ian', 1, 'ian@email.com', '5');
 
     expect(manager.name).tobe('Ian');
