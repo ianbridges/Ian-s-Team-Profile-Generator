81f889c47293a785a4824654530fe5c7e4066ed0
ianbridges <megabridge12@gmail.com>
2022-10-10 14:54:55 -0500

    index.js and style add

--- index.js
+++ index.js
@@ -1,56 +1,167 @@
-import inquirer from "inquirer";
-import fs from "fs";
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-import Engineer from '../Ian-s-Team-Profile-Generator/lib/Engineer.js'
-import Intern from '../Ian-s-Team-Profile-Generator/lib/Intern.js'
-import Manager from '../Ian-s-Team-Profile-Generator/lib/Manager.js'
+const Manager = require ('../Ian-s-Team-Profile-Generator/lib/Manager');
+const Engineer = require ('../Ian-s-Team-Profile-Generator/lib/Engineer');
+const Intern = require ('../Ian-s-Team-Profile-Generator/lib/Intern');
+const renderData = require ('../Ian-s-Team-Profile-Generator/src/renderData');
 
-import renderData from '../Ian-s-Team-Profile-Generator/src/renderData.js'
-import Choices from "inquirer/lib/objects/choices.js";
+var employees = [];
 
 const questions = [
     {
         type: 'input',
         name: 'employeeName',
-        message: "What is the employee's name?"
+        message: "Employee's name?"
+    },
+    {
+        type: 'list',
+        name: 'employeeTitle',
+        message: "Employee's title?",
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern'
+        ]
     },
     {
         type: 'input',
         name: 'employeeId',
-        message: "What is the employee's id?"
+        message: "Employee's id?"
     },
     {
         type: 'input',
         name: 'employeeEmail',
-        message: "What is the employee's email?"
-    }
-];
+        message: "Employee's email?"
+    }];
 
-const engineerQuestion = {
+const managerQuestion = {
     type: 'input',
-    name: 'git User',
-    message: "What is the engineeer's username on github?"
+    name: 'office',
+    message: "Manager's office number?"
+};
+
+const exitQuestion = {
+    type: 'list',
+    name: 'chooseNext',
+    message: "What's next?",
+    choices: [
+        'Add Engineer',
+        'Add Intern',
+        'Exit'
+    ]
 };
 
 const internQuestion = {
     type: 'input',
     name: 'school',
-    message: "What school did the intern attend?"
-}
-
-const managerQuestion = {
-    type: 'input',
-    name: 'office',
-    message: "What is the manger's office number?"
+    message: "Intern's school?"
 };
 
-const exitQuestion = {
+const engineerQuestion = {
     type: 'input',
-    name: 'chooseNext',
-    message: "What would you like to do now?",
-    choices: [
-       'Add Engineer',
-       'Add Intern',
-       'Exit'
-    ]
+    name: 'gUser',
+    message: "Engineer's github?"
+};
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log("File written")
+    })
+};
+
+function init() {
+    var managerQuestions = [...questions, managerQuestion, exitQuestion]
+    inquirer.prompt(managerQuestions)
+        .then((answers) => {
+            var manager = new Manager(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.office);
+            employees.push(manager)
+            if (
+                answers.chooseNext === 'Add Engineer'
+            ) {
+                addEngineer();
+            } else if (
+                answers.chooseNext === 'Add Intern'
+            ) {
+                addIntern();
+            } else if (
+                answers.chooseNext === 'Exit'
+            ) {
+                var data = renderData(employees);
+                writeToFile("dist/index.html", data);
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log("Prompt failed to render")
+            } else {
+                console.log("Something else went wrong")
+            }
+        });
 }
+
+function addEngineer() {
+    var engineerQuestions = [...questions, engineerQuestion, exitQuestion]
+    inquirer.prompt(engineerQuestions)
+        .then((answers) => {
+            var engineer = new Engineer(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.gUser);
+            employees.push(engineer);
+            if (
+                answers.chooseNext === 'Add Engineer'
+            ) {
+                addEngineer();
+            } else if (
+                answers.chooseNext === 'Add Intern'
+            ) {
+                addIntern();
+            } else if (
+                answers.chooseNext === 'Exit'
+            ) {
+                var data = renderData(employees);
+                writeToFile("dist/index.html", data);
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log("Prompt failed to render")
+            } else {
+                console.log("Something else went wrong")
+            }
+        });
+};
+
+function addIntern() {
+    var internQuestions = [...questions, internQuestion, exitQuestion]
+    inquirer.prompt(internQuestions)
+        .then((answers) => {
+            var intern = new Intern(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.school);
+            employees.push(intern);
+            if (
+                answers.chooseNext === 'Add Engineer'
+            ) {
+                addEngineer();
+            } else if (
+                answers.chooseNext === 'Add Intern'
+            ) {
+                addIntern();
+            } else if (
+                answers.chooseNext === 'Exit'
+            ) {
+                var data2 = renderData(employees);
+                writeToFile("dist/index.html", data2);
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log("Prompt failed to render")
+            } else {
+                console.log("Something else went wrong")
+            }
+        });
+};
+
+
+init();
